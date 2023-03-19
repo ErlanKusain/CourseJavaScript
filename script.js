6c89248224e38bc3306540324d1941e720059c75
erlankusain <erlankusain@yandex.ru>
2023-03-19 09:02:40 +0600

    start commit

---
+++ script.js
@@ -0,0 +1,74 @@
+// let number = 5;
+// const leftBorderWidth = 1;
+
+// number = 90;
+// console.log(number);
+
+
+
+//Объекты
+
+// const erlanKusain = {
+//     name: 'erlan',
+//     age: 25,
+//     city: 'Pavlodar',
+//     isMarried: true,
+//     child: false
+// };
+
+// const object = {
+//     свойство: значение,
+//     property: value,
+//     property: value,
+//     property: value
+// }
+
+
+
+
+//Массивы:
+
+//             пор. номер:  property: property: property:
+//                   0        1         2        3
+// const array = ["значение", "value", "value", "value"]
+
+
+
+//Объекты:
+
+// const erlanKusain = {
+//     name: 'erlan',
+//     firstName: 'kusain',
+//     age: 24
+// }
+// const elamanKusain = {
+//     name: 'elaman',
+//     firstName: 'kusain',
+//     age: 23
+// }
+
+
+
+//Добавление свойств в обьекты через точку и скобки:
+
+// erlanKusain.city = 'Pavlodar';
+// elamanKusain.city = 'Pavlodar';
+// console.log(elamanKusain)
+
+
+//Всплывающие окна(Три брата акрабата)
+
+// alert('Allahu Akbar');
+
+// confirm('Are you ready?')
+
+// prompt("Вам есть 18?")
+
+
+const answers = [];
+
+answers[0] = prompt('Вы готовы?', '');
+answers[1] = prompt('Старт?', '');
+answers[2] = prompt('Начать', '');
+
+document.write(answers);
